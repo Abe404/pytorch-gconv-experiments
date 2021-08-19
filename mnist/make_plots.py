8a341fe024ac67c90c7c82d196d3bb560207339e
Abraham George Smith <abe@abesmith.co.uk>
2021-08-19 17:26:43 +0200

    wip plot time

--- mnist/make_plots.py
+++ mnist/make_plots.py
@@ -3,7 +3,41 @@ import seaborn as sns
 from csv_utils import load_csv
 import os
 
-def plot_acc():
+
+def plot_acc_time():
+    # First plot dice over epochs for both training and validation.
+    plt.figure(figsize=(16, 9))
+    clrs = sns.color_palette("husl", 5)
+    with sns.axes_style('white'):
+        plt.grid()
+        plt.xticks(range(20))
+        plt.ylim([92, 100])
+        plt.ylabel('accuracy %')
+        plt.xlabel('time')
+
+        for net in ['group', 'no_group']:
+            for i in range(10):
+                run = str(i).zfill(2)
+                log_file = f'logs/cnn_{net}_{run}.csv' 
+                (epochs, accs, _, _) = load_csv(log_file,
+                                                ['epoch', 'test_accuracy',
+                                                'start_time', 'cur_time'],
+                                                [int, float, float, float])
+    
+                if net == 'group':
+                    plt.plot(epochs, accs, label=f'test accuracy {net} {run}, max: {round(max(accs), 3)}', linestyle='--')
+                else:
+                    plt.plot(epochs, accs, label=f'test accuracy {net} {run}, max: {round(max(accs), 3)}')
+
+    plt.legend()
+    #fig_path = os.path.join('plots', f'{net}_{run}.png')
+    fig_path = os.path.join('plots', f'epochs_group_vs_no_group.png')
+    print('saving figure to ', fig_path)
+    plt.savefig(fig_path)
+
+
+
+def plot_acc_epochs():
     # First plot dice over epochs for both training and validation.
     plt.figure(figsize=(16, 9))
     clrs = sns.color_palette("husl", 5)
@@ -27,9 +61,10 @@ def plot_acc():
                     plt.plot(epochs, accs, label=f'test accuracy {net} {run}, max: {round(max(accs), 3)}', linestyle='--')
                 else:
                     plt.plot(epochs, accs, label=f'test accuracy {net} {run}, max: {round(max(accs), 3)}')
+
     plt.legend()
     #fig_path = os.path.join('plots', f'{net}_{run}.png')
-    fig_path = os.path.join('plots', f'group_vs_no_group.png')
+    fig_path = os.path.join('plots', f'epochs_group_vs_no_group.png')
     print('saving figure to ', fig_path)
     plt.savefig(fig_path)
 
