227d3b87584af1610fe40ac99f0405a049f18292
Abraham George Smith <abe@abesmith.co.uk>
2021-08-19 14:13:42 +0200

    added mnist with comparison between group and not group convolution cnn with logs saved as csv for comparison later

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -8,6 +8,9 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4
 from groupy.gconv.pytorch_gconv.pooling import plane_group_spatial_max_pooling
+from torch.nn import MaxPool2d
+import time
+
 
 
 # Training settings
@@ -16,25 +19,20 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
-
+#torch.manual_seed(args.seed)
+#if args.cuda:
+#    torch.cuda.manual_seed(args.seed)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
@@ -52,9 +50,9 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
 
-class Net(nn.Module):
+class GroupNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(GroupNet, self).__init__()
         self.conv1 = P4ConvZ2(1, 10, kernel_size=3)
         self.conv2 = P4ConvP4(10, 10, kernel_size=3)
         self.conv3 = P4ConvP4(10, 20, kernel_size=3)
@@ -70,19 +68,43 @@ class Net(nn.Module):
         x = F.relu(self.conv4(x))
         x = plane_group_spatial_max_pooling(x, 2, 2)
         x = x.view(x.size()[0], -1)
+        # print('GroupNet: fc1 x shape = ', x.shape) # 64x1280
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x)
 
-model = Net()
-if args.cuda:
-    model.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, 3)
+        self.conv2 = nn.Conv2d(10, 10, 3)
+        self.conv3 = nn.Conv2d(10, 20, 3)
+        self.conv4 = nn.Conv2d(20, 20, 3)
 
+        # self.fc1 = nn.Linear(4*4*20*4, 50)
+        self.fc1 = nn.Linear(4*4*20, 50) # reduced parameters in the fully convolutional layers.
+        self.fc2 = nn.Linear(50, 10)
 
-def train(epoch):
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(x.size()[0], -1)
+
+        #untimeError: mat1 and mat2 shapes cannot be multiplied (64x320 and 1280x50)
+        # print('Net: fc1 x shape = ', x.shape) # 64x320
+
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+def train(model, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         if args.cuda:
@@ -94,12 +116,11 @@ def train(epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} '.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+                100. * batch_idx / len(train_loader), loss.item()), end='\r')
 
-
-def test():
+def test(model):
     model.eval()
     test_loss = 0
     correct = 0
@@ -108,16 +129,44 @@ def test():
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+        test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(test_loader.dataset)
+
+    acc_percent = 100.0 * correct / len(test_loader.dataset)
+
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    return acc_percent.item()
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test()
+def plot_metrics(NetClass, log_file, epochs):
+    model = NetClass()
+    if args.cuda:
+        model.cuda()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    start = time.time()
+    for epoch in range(1, epochs + 1):
+        train(model, optimizer, epoch)
+        acc_percent = test(model)
+        print(f'{epoch},{round(acc_percent, 4)},{start},{time.time()}', file=log_file)
+
+
+def plot_all_metrics():
+    epochs = 20
+    for i in range(10):
+        train_log = open('logs/cnn_no_group_' + str(i).zfill(2) + '.csv', 'w+')
+        print('epoch,test_accuracy,start_time,cur_time', file=train_log)
+        plot_metrics(Net, train_log, epochs)
+
+    for i in range(10):
+        train_log = open('logs/cnn_group_' + str(i).zfill(2) + '.csv', 'w+')
+        print('epoch,test_accuracy,start_time,cur_time', file=train_log)
+        plot_metrics(GroupNet, train_log, epochs)
+
+
+if __name__ == '__main__':
+    plot_all_metrics()
